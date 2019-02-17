138dea80fa0b5d61386931975fb4a848d369dee8
nisten <nisten@outlook.com>
2019-02-17 13:53:57 -0500

    static-rendering enabled

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,17 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/jag/work/react-static-elastic/node_modules/react-static-plugin-typescript",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "/Users/jag/work/react-static-elastic",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
